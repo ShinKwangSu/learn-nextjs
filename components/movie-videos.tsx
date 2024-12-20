ba35b67e902145dc06c8d2600685be9e5a59fb05
신광수 <kwangsu.shin@saltlux.com>
2024-12-20 18:01:35 +0900

    API_URL seperate

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,8 +1,9 @@
-import { API_URL } from "../app/(home)/page";
+
+import { API_URL } from "../app/constants";
 import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
+  const response = await fetch(`${API_URL}/${id}/videos`, { cache: 'force-cache' });
   return response.json();
 }
 
